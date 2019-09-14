5cb65d9cabc866b5c280e5bd230a9c18037bbc56
Tetyana Duchevych <duchevich274@gmail.com>
2019-09-14 17:15:17 +0300

    les 52 fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('star db')
+// var 1
+fetch('https://swapi.co/api/people/1/')
+    .then((res) => {
+        return res.json();
+    })
+    .then((body) => {
+        console.log(body);
+    });
+
+// var 2 (более читаемая запись var 1)
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+}
+
+getResource('https://swapi.co/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });
