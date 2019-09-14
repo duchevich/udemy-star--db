9184fb9615dc53d923301e45cd56cf106fd71035
Tetyana Duchevych <duchevich274@gmail.com>
2019-09-14 20:02:45 +0300

    les 56 component, who gets data from API

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -7,8 +7,7 @@ export default class PersonDetails extends Component {
   render() {
     return (
       <div className="person-details card">
-        <img className="person-image" src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
-
+        <img alt="" className="person-image" src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
         <div className="card-body">
           <h4>R2-D2</h4>
           <ul className="list-group list-group-flush">
