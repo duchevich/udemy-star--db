8f91e0ed9ef5fee6c26436c13fd7b6efafd29c5f
Tetyana Duchevych <duchevich274@gmail.com>
2019-09-14 17:26:18 +0300

    les 53 errors in fetch API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-// var 1
-fetch('https://swapi.co/api/people/1/')
-    .then((res) => {
-        return res.json();
-    })
-    .then((body) => {
-        console.log(body);
-    });
-
-// var 2 (более читаемая запись var 1)
 const getResource = async (url) => {
     const res = await fetch(url);
+    if(!res.ok){
+        throw new Error (`Could not fetch ${url}, 
+        received ${res.status}`);
+    }
     const body = await res.json();
     return body;
 }
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/1234234/')
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch ', err);
     });
