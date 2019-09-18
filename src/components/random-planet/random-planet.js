14f16f4cb78294227965c2ad44c4df8f14c0157c
Tetyana Duchevych <duchevich274@gmail.com>
2019-09-18 23:04:05 +0300

    les 64 componentDidMount()

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -15,16 +15,10 @@ export default class RandomPlanet extends Component {
 		loading: true
 	};
 
-	constructor() {
-		super();
-		console.log('constructor()');
+	componentDidMount(){
 		this.updatePlanet();
 		this.interval = setInterval(this.updatePlanet, 10000);
 		// clearInterval(this.interval);
-	}
-
-	componentDidMount(){
-		console.log('componentDidMount()');
 	}
 	componentWillUnmount(){
 		console.log('componentWillUnmount()');
