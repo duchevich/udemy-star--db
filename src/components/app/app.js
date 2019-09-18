5fa3c2e7ca3326478922b03dbf08470aaedfb51f
Tetyana Duchevych <duchevich274@gmail.com>
2019-09-18 23:00:17 +0300

    les 63 component life circle

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,44 +9,40 @@ import './app.css';
 
 export default class App extends Component {
 
-  state = {
-    showRandomPlanet: true
-  };
+	state = {
+		showRandomPlanet: true
+	};
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
+	toggleRandomPlanet = () => {
+		this.setState((state) => {
+		return {
+			showRandomPlanet: !state.showRandomPlanet
+		}
+		});
+	};
 
-  render() {
+	render() {
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+		const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
-    return (
-      <div className="stardb-app">
-        <Header />
-        { planet }
-
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
-          Toggle Random Planet
-        </button>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails />
-          </div>
-        </div>
-      </div>
-    );
-  }
+		return (
+		<div className="stardb-app">
+			<Header />
+			{ planet }
+			<button
+				className="toggle-planet btn btn-warning btn-lg"
+				onClick={this.toggleRandomPlanet}>
+				Toggle Random Planet
+			</button>
+			<div className="row mb2">
+				<div className="col-md-6">
+					<ItemList />
+				</div>
+				<div className="col-md-6">
+					<PersonDetails />
+				</div>
+			</div>
+		</div>
+		);
+	}
 }
