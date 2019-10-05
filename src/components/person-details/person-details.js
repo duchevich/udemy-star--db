ed641fac7067f32c41949319ceb9f853f7e811b7
Tetyana Duchevych <duchevich274@gmail.com>
2019-10-05 21:48:51 +0300

    les 67 Используем на практике componentDidUpdate()

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,31 +1,103 @@
 import React, { Component } from 'react';
 
 import './person-details.css';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 export default class PersonDetails extends Component {
+	swapiService = new SwapiService();
 
-  render() {
-    return (
-      <div className="person-details card">
-        <img alt="" className="person-image" src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
-        <div className="card-body">
-          <h4>R2-D2</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>male</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>red</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
+	state = {
+		person: null,
+		loading: true
+	}
+
+	componentDidMount(){
+		this.updatePerson();
+	}
+
+	componentDidUpdate(prevProps){
+		if(this.props.personId !== prevProps.personId){
+			this.setState({
+				loading: true,
+			})
+			this.updatePerson();
+		}
+	}
+
+	updatePerson(){
+		const {personId} = this.props;
+		if (!personId) {
+			return;
+		}
+		this.swapiService
+		.getPerson(personId)
+		.then((person) => {
+			this.setState({
+				person,
+				loading: false,
+				error: false
+			})
+		})
+		.catch(this.onError);
+	}
+
+	onError = (err) => {
+		this.setState({
+		error: true,
+		loading: false
+		});
+	};
+	render() {
+
+		if(!this.state.person){
+			return <span>Select a person from a list</span>;
+		}
+
+		const { person, loading, error } = this.state;
+
+		const hasData = !(loading || error);
+
+		const errorMessage = error ? <ErrorIndicator/> : null;
+		const spinner = loading ? <Spinner /> : null;
+		const content = hasData ? <PersonView person={person}/> : null;
+
+		return (
+		<div className="person-details card">
+			{errorMessage}
+			{spinner}
+			{content}
+		</div>
+		)
+	}
 }
+
+const PersonView = ({ person }) => {
+
+	const { id, name, gender, birthYear, eyeColor } = person;
+
+	return (
+		<React.Fragment>
+			<img alt="character" className="person-image" src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+			<div className="card-body">
+			<h4>{name}</h4>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">
+				<span className="term">Genderrr</span>
+				<span>{gender}</span>
+				</li>
+				<li className="list-group-item">
+				<span className="term">Birth Year</span>
+				<span>{birthYear}</span>
+				</li>
+				<li className="list-group-item">
+				<span className="term">Eye Color</span>
+				<span>{eyeColor}</span>
+				</li>
+			</ul>
+			</div>
+		</React.Fragment>
+	);
+};
+
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -39,7 +39,7 @@ export default class RandomPlanet extends Component {
 	};
 
 	updatePlanet = () => {
-		console.log('update');
+		// console.log('update');
 		const id = Math.floor(Math.random()*17) + 2;
 		this.swapiService
 		.getPlanet(id)
@@ -48,7 +48,7 @@ export default class RandomPlanet extends Component {
 	};
 
 	render() {
-		console.log('render()');
+		// console.log('render()');
 
 		const { planet, loading, error } = this.state;
 
