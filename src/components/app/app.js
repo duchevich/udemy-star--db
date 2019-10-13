5cfeaa53cd52ea48dfb3fda3df31652e66a53c48
Tetyana Duchevych <duchevich274@gmail.com>
2019-10-13 10:58:04 +0300

    les 77 Работа с props.children

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,7 @@ import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page';
 import ItemList from '../item-list/item-list';
-import ItemDetails from '../item-details/item-details';
+import ItemDetails, { Record } from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
 import Row from '../row/row';
 
@@ -41,10 +41,25 @@ export default class App extends Component {
 		null;
 
 		const personDetails = (
-			<ItemDetails itemId={11} getData={this.swapiService.getPerson} getImageUrl={this.swapiService.getPersonImage}/>
+			<ItemDetails 
+				itemId={11}
+				getData={this.swapiService.getPerson} 
+				getImageUrl={this.swapiService.getPersonImage}>
+
+				<Record field="gender" label="Gender" />
+				<Record field="eyeColor" label="Eye Color" />
+			</ItemDetails>
 		)
 		const starshipDetails = (
-			<ItemDetails itemId={5} getData={this.swapiService.getStarship} getImageUrl={this.swapiService.getStarshipImage}/>
+			<ItemDetails 
+				itemId={5} 
+				getData={this.swapiService.getStarship} 
+				getImageUrl={this.swapiService.getStarshipImage}>
+					<Record field="model" label="Model" />
+					<Record field="length" label="Length" />
+					<Record field="costInCredits" label="Cost" />
+			</ItemDetails>
+
 		)
 
 		return (
--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -6,6 +6,20 @@ import ErrorButton from "../error-button/error-button";
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
 
+
+const Record = ({item, field, label}) => {
+	return (
+		<li className="list-group-item">
+				<span className="term">{label}</span>
+				<span>{field}</span>
+				</li>
+	)
+}
+
+export {
+	Record
+};
+
 export default class ItemDetails extends Component {
 
 	swapiService = new SwapiService();
@@ -54,25 +68,33 @@ export default class ItemDetails extends Component {
 	};
 	render() {
 
-		if(!this.state.item){
+		const { item, image } = this.state;
+		if (!item) {
 			return <span>Select a item from a list</span>;
 		}
-
-		const { item, image, loading, error } = this.state;
-
-		const hasData = !(loading || error);
-
-		const errorMessage = error ? <ErrorIndicator/> : null;
-		const spinner = loading ? <Spinner /> : null;
-		const content = hasData ? <ItemView item={item} image={image}/> : null;
-
+	
+		const { id, name, gender,
+					birthYear, eyeColor } = item;
+	
 		return (
-		<div className="item-details card">
-			{errorMessage}
-			{spinner}
-			{content}
-		</div>
-		)
+			<div className="item-details card">
+				<img className="item-image"
+					src={image}
+					alt="item"/>
+		
+				<div className="card-body">
+					<h4>{name}</h4>
+					<ul className="list-group list-group-flush">
+					{
+						React.Children.map(this.props.children, (child) => {
+						return React.cloneElement(child, { item });
+						})
+					}
+					</ul>
+					<ErrorButton />
+				</div>
+			</div>
+		);
 	}
 }
 
@@ -86,18 +108,7 @@ const ItemView = ({ item, image }) => {
 			<div className="card-body">
 			<h4>{name}</h4>
 			<ul className="list-group list-group-flush">
-				<li className="list-group-item">
-				<span className="term">Gender</span>
-				<span>{gender}</span>
-				</li>
-				<li className="list-group-item">
-				<span className="term">Birth Year</span>
-				<span>{birthYear}</span>
-				</li>
-				<li className="list-group-item">
-				<span className="term">Eye Color</span>
-				<span>{eyeColor}</span>
-				</li>
+				{this.props.children}
 			</ul>
 			<ErrorButton />
 			</div>
