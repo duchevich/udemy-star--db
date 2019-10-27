df602f0798f3813ca2b1defe72871b81e27d6192
Tetyana Duchevych <duchevich274@gmail.com>
2019-10-27 16:01:09 +0200

    les 98. Динамические пути

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,7 +9,7 @@ import DummySwapiService from '../../services/dummy-swapi-service';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import StarshipDetails from "../sw-components/starship-details";
 import './app.css';
 
 export default class App extends Component {
@@ -41,7 +41,12 @@ export default class App extends Component {
 									render={() => <h2>People</h2>}/>
 							<Route path="/people" component={PeoplePage}/>
 							<Route path="/planets" component={PlanetsPage}/>
-							<Route path="/starships" component={StarshipsPage}/>
+							<Route path="/starships" exact component={StarshipsPage}/>
+							<Route path="/starships/:id" 
+									render={({match}) => {
+										const { id } = match.params;
+										return <StarshipDetails itemId={id}/>
+									}} />
 						</div>
 					</Router>
 				</SwapiServiceProvider>
