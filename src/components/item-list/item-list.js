bbe962eb6eb83f9b003f36e6e1b169b303c8969a
Tetyana Duchevych <duchevich274@gmail.com>
2019-10-27 13:24:14 +0200

    les 90. Свойство defaultProps

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './item-list.css';
 
 const  ItemList = (props) => {
+
 	const { data, onItemSelected, children: renderLabel } = props;
+
 	const items = data.map((item) => {
 		const {id} = item;
 		const label = renderLabel(item);
@@ -14,12 +16,17 @@ const  ItemList = (props) => {
 			</li>
 		);
 	});
+
 	return (
 		<ul className="item-list list-group">
 			{items}
 		</ul>
 	);
 
+}
+
+ItemList.defaultProps = {
+	onItemSelected: () => {}
 }
 
 export default ItemList;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -7,7 +7,9 @@ import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
-
+	static defaultProps = {
+		updateInterval: 10000
+	}
 	swapiService = new SwapiService();
 
 	state = {
@@ -16,8 +18,9 @@ export default class RandomPlanet extends Component {
 	};
 
 	componentDidMount() {
+		const {updateInterval} = this.props;
 		this.updatePlanet();
-		this.interval = setInterval(this.updatePlanet, 10000);
+		this.interval = setInterval(this.updatePlanet, updateInterval);
 	}
 
 	componentWillUnmount() {
