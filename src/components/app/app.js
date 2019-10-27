542adf06fed5dfb5d5393ad0e09ba827f9acb112
Tetyana Duchevych <duchevich274@gmail.com>
2019-10-27 17:20:09 +0200

    les 100. Относительные пути

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -41,7 +41,7 @@ export default class App extends Component {
 									render={() => <h2>People</h2>}/>
 							<Route path="/people" component={PeoplePage}/>
 							<Route path="/planets" component={PlanetsPage}/>
-							<Route path="/starships" exact component={StarshipsPage}/>
+							<Route path="/starships/" exact component={StarshipsPage}/>
 							<Route path="/starships/:id" 
 									render={({match}) => {
 										const { id } = match.params;
